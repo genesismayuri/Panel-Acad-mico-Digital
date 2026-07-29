5248985037704abc42e5bfc7ea2c8fd9e92adfdc
betzabxscobar <betzabxscobar@gmail.com>
2026-07-29 09:31:18 -0500

    Diseño de pagina

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,133 @@
 import { buscarUniversidades } from "./api.js";
+import { guardarSolicitud, obtenerSolicitudes } from "./storage.js";
 
-form.addEventListener("submit", registrarSolicitud);
+
+const form = document.getElementById("formularioSolicitud");
+
+const botonBuscar = document.getElementById("botonBuscar");
+
 
 
 async function consultarUniversidades(){
 
     const pais = document.getElementById("pais").value;
 
+
     const universidades = await buscarUniversidades(pais);
 
-    console.log(universidades);
+
+    const resultado = document.getElementById("resultadoAPI");
+
+
+    resultado.innerHTML = "";
+
+
+    universidades.forEach(universidad => {
+
+        resultado.innerHTML += `
+
+        <div class="tarjeta">
+
+            <h3>${universidad.name}</h3>
+
+            <p>
+            País: ${universidad.country}
+            </p>
+
+            <p>
+            Dominio:
+            ${universidad.domains[0] || "No disponible"}
+            </p>
+
+        </div>
+
+        `;
+
+    });
 
 }
 
 
-botonBuscar.addEventListener("click", consultarUniversidades);
+
+function registrarSolicitud(e){
+
+    e.preventDefault();
+
+
+    const solicitud = {
+
+        nombre: document.getElementById("nombre").value,
+
+        correo: document.getElementById("correo").value,
+
+        edad: document.getElementById("edad").value,
+
+        tipo: document.getElementById("tipo").value,
+
+        descripcion: document.getElementById("descripcion").value
+
+    };
+
+
+    guardarSolicitud(solicitud);
+
+
+    document.getElementById("mensaje").textContent =
+    "Solicitud guardada correctamente ";
+
+
+    mostrarSolicitudes();
+
+}
+
+function mostrarSolicitudes(){
+
+    const contenedor = document.getElementById(
+        "solicitudesGuardadas"
+    );
+
+
+    const solicitudes = obtenerSolicitudes();
+
+
+    contenedor.innerHTML = "";
+
+
+    solicitudes.forEach(solicitud => {
+
+
+        contenedor.innerHTML += `
+
+        <div class="tarjeta">
+
+            <h3>${solicitud.nombre}</h3>
+
+            <p>${solicitud.tipo}</p>
+
+            <p>${solicitud.descripcion}</p>
+
+        </div>
+
+        `;
+
+
+    });
+
+}
+
+botonBuscar.addEventListener(
+"click",
+consultarUniversidades
+);
+
+
+form.addEventListener(
+"submit",
+registrarSolicitud
+);
+
+
+document.addEventListener(
+"DOMContentLoaded",
+mostrarSolicitudes
+);
---
+++ JS/storage.js
@@ -0,0 +1,25 @@
+export function guardarSolicitud(solicitud){
+
+    let solicitudes = JSON.parse(
+        localStorage.getItem("solicitudes")
+    ) || [];
+
+
+    solicitudes.push(solicitud);
+
+
+    localStorage.setItem(
+        "solicitudes",
+        JSON.stringify(solicitudes)
+    );
+
+}
+
+
+export function obtenerSolicitudes(){
+
+    return JSON.parse(
+        localStorage.getItem("solicitudes")
+    ) || [];
+
+}
